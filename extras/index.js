be4bfbebb07d0af467e9997498df346eec572dea
Mayank Shekhar <mayankshekhar03@gmail.com>
2017-06-22 17:38:10 +0530

    linked the gps info with the api

--- extras/index.js
+++ extras/index.js
@@ -1,33 +1,22 @@
-$(document).ready(function(){
-   /* $.ajax({
-    url: 'https://simple-weather.p.mashape.com/weather?lat=26.8467&lng=80.9462', // The URL to the API. You can get this in the API page of the API you intend to consume
-    type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
-    data: {}, // Additional parameters here
-    dataType: 'text/plain',
-    success: function(data) { document.getElementById('weather').innerHTML = data.source ; },
-    error: function(err) { alert(err); },
-    beforeSend: function(xhr) {
-    xhr.setRequestHeader("X-Mashape-Authorization", "dNnULvXBm5mshHJ6MJqtHYoa98Bop16nooWjsnhv9HCEBHvpy5"); // Enter here your Mashape key
-    }
-}); */
-    
-    
-    var link = navigator.geolocation.getCurrentPosition(function(position) {
-        return 'https://simple-weather.p.mashape.com/weather?lat=' + position.coords.latitude + '&lng=' +  position.coords.longitude;
-    });
-    
-    console.log(link);
-    
+function getLink(){
+    navigator.geolocation.getCurrentPosition(getPosition);
+}
+
+function getPosition(position) {
+    var l = 'https://simple-weather.p.mashape.com/weather?lat=' + String(position.coords.latitude) + '&lng=' + String(position.coords.longitude);
     $.ajax({
         headers: {
           "X-Mashape-Key": "dNnULvXBm5mshHJ6MJqtHYoa98Bop16nooWjsnhv9HCEBHvpy5",
           Accept: "text/plain",
           "Content-Type": "application/x-www-form-urlencoded"
         },
-        url: link,
+        url: l,
         success: function(r) {
             document.getElementById('weather').innerHTML = r;
         },
         error: function(err) { alert(err); }
     });
+}
+$(document).ready(function(){
+    getLink();
 });
